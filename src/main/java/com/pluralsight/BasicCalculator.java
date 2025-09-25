ac96fc4ba765e93989ea373489f824266ae4c5a1
nati-tewolde <natisnewemail@gmail.com>
2025-09-25 11:02:34 -0400

    Completed Mod3 - Exercise 3 - Basic Calculator

--- src/main/java/com/pluralsight/BasicCalculator.java
+++ src/main/java/com/pluralsight/BasicCalculator.java
@@ -1,7 +1,20 @@
 package com.pluralsight;
 
+import java.util.Scanner;
+
 public class BasicCalculator {
     public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter the first number: ");
+        int firstNum = input.nextInt();
+
+        System.out.print("Enter the second number: ");
+        int secondNum = input.nextInt();
+
+        int multipliedNum = firstNum * secondNum;
+        System.out.println(firstNum + " * " + secondNum + " = " + multipliedNum);
 
     }
 }
